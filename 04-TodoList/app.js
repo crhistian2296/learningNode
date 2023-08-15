1e66ae775930791453a47c14dfb2fe38b8a8d4e2
crhistian2296 <garcia.crhistian@correounivalle.edu.co>
2023-08-15 23:44:51 +0200

    inquirer version

--- 04-TodoList/app.js
+++ 04-TodoList/app.js
@@ -1,13 +1,15 @@
 require('colors');
 
-const { showMenu, pause } = require('./helpers/messages');
+const { inquirerMenu } = require('./helpers/inquierer');
+// const { showMenu, pause } = require('./helpers/messages');
 
 const main = async () => {
   let opt = '';
 
   do {
-    opt = await showMenu();
-    if (opt !== '0') await pause(`Press ${'enter'.blue} to continue: `);
+    opt = await inquirerMenu();
+    // if (opt !== '0') await pause(`Press ${'enter'.blue} to continue: `);
+    console.log(opt);
   } while (opt !== '0');
 };
 
---
+++ 04-TodoList/helpers/inquierer.js
@@ -0,0 +1,34 @@
+const inquirer = require('inquirer');
+require('colors');
+
+const options = [
+  {
+    type: 'list',
+    name: 'option',
+    messages: 'What is your desire?',
+    choices: [
+      { value: '1', name: `${'1.'.blue} Add Todo` },
+      { value: '2', name: `${'2.'.blue} Show Todos` },
+      { value: '3', name: `${'3.'.blue} Show complete Todos` },
+      { value: '4', name: `${'4.'.blue} Show incomplete Todos` },
+      { value: '5', name: `${'5.'.blue} Complete Todos` },
+      { value: '6', name: `${'6.'.blue} Delete Todos` },
+      { value: '0', name: `${'0.'.blue} Exit` },
+    ],
+  },
+];
+
+const inquirerMenu = async () => {
+  // console.clear();
+
+  // Header
+  console.log('============================='.rainbow);
+  console.log('       Choose an option      '.white);
+  console.log('=============================\n'.rainbow);
+
+  const { option } = await inquirer.prompt(options);
+
+  return option;
+};
+
+module.exports = { inquirerMenu };
