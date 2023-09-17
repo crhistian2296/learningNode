f5aa6374be1dcdf88c0f55e98f1afd7f9258cd07
crhistian2296 <garcia.crhistian@correounivalle.edu.co>
2023-09-17 14:41:08 +0200

    feat: validacion lectura db

--- 04-TodoList/helpers/dbInteractions.js
+++ 04-TodoList/helpers/dbInteractions.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { pause } = require('./inquierer');
 
 const file = './db/data.json';
 
@@ -7,7 +8,13 @@ const saveToDB = (data) => {
 };
 
 const readtoDB = () => {
-  if (!fs.readFileSync(file)) return null;
+  try {
+    fs.readFileSync(file);
+  } catch (err) {
+    pause();
+    return null;
+  }
+
   const data = fs.readFileSync(file, { encoding: 'utf-8' });
   const parsedData = JSON.parse(data);
 
