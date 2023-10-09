b9ce3a0b008ff3e794fe714602bf9f32924485ea
crhistian2296 <garcia.crhistian@correounivalle.edu.co>
2023-10-09 14:49:58 +0200

    feat: template website

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -2,14 +2,23 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+// SSR
+// se hace uso del filesystem para servir el html, sino hay coincidencia se
+// busca en las definicion de cada ruta (line 12-24)
+
+app.use(express.static('public'));
+
+app.get('/elements', (req, res) => {
+  res.sendFile(`${__dirname}/public/elements.html`);
+});
+app.get('/generic', (req, res) => {
+  res.sendFile(`${__dirname}/public/generic.html`);
 });
 app.get('/hola-mundo', (req, res) => {
   res.send('Hello World in 8080 port');
 });
 app.get('*', (req, res) => {
-  res.send('404 | Page not Found');
+  res.sendFile(`${__dirname}/public/404.html`);
 });
 
 app.listen(port, () => {
