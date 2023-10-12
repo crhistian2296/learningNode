a2c6e1e074c9522770cf82d94b08d930a5fc8be0
crhistian2296 <garcia.crhistian@correounivalle.edu.co>
2023-10-12 15:23:26 +0200

    feat: preparation to deploy

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,25 +1,37 @@
 const express = require('express');
-const app = express();
-const port = 8080;
+const hbs = require('hbs');
+require('dotenv').config();
 
-// SSR
+const app = express();
+const port = process.env.PORT;
+
+// Servir contenido estatico
 // se hace uso del filesystem para servir el html, sino hay coincidencia se
 // busca en las definicion de cada ruta (line 12-24)
-
 app.use(express.static('public'));
 
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(`${__dirname}/views/partials`);
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    name: 'Crhistian',
+    study: 'Node - Express',
+  });
+});
 app.get('/elements', (req, res) => {
-  res.sendFile(`${__dirname}/public/elements.html`);
+  res.render('elements');
 });
 app.get('/generic', (req, res) => {
-  res.sendFile(`${__dirname}/public/generic.html`);
+  res.render('generic');
 });
 app.get('/hola-mundo', (req, res) => {
   res.send('Hello World in 8080 port');
 });
-app.get('*', (req, res) => {
-  res.sendFile(`${__dirname}/public/404.html`);
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(`${__dirname}/public/404.html`);
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
